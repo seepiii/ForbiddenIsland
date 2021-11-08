2dbb3f42d844e18b85bd42c2d6252534d8519f4f
cc594 <93924271+cc594@users.noreply.github.com>
2021-11-08 07:45:24 -0600

    Update ForbiddenIslandRunner.java

--- ForbiddenIslandRunner.java
+++ ForbiddenIslandRunner.java
@@ -1,5 +1,6 @@
 
 public class ForbiddenIslandRunner {
+	private int seed, waterLevel, numberOfPlayers;
 	public static void main(String[] args) {
 		ForbiddenIslandFrame frame = new ForbiddenIslandFrame("Forbidden Island");
 		
