2e529624c14d56a799e1ea81f433b8da97d7182c
seepiii <93924225+seepiii@users.noreply.github.com>
2021-11-08 07:33:15 -0600

    Add files via upload

---
+++ ForbiddenIsland.java
@@ -0,0 +1,7 @@
+
+public class ForbiddenIsland {
+	public static void main(String[] args) {
+		ForbiddenIslandFrame frame = new ForbiddenIslandFrame("Forbidden Island");
+		
+	}
+}
---
+++ ForbiddenIslandFrame.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+public class ForbiddenIslandFrame extends JFrame {
+	private static final int WIDTH = 1600;
+	private static final int HEIGHT = 960;
+	public ForbiddenIslandFrame(String title) {
+		super(title);
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
