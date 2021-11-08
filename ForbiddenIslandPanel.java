06a4baf5fcc4cf79123613da4f3c296322696527
cc594 <93924271+cc594@users.noreply.github.com>
2021-11-08 07:57:03 -0600

    Update ForbiddenIslandPanel.java

--- ForbiddenIslandPanel.java
+++ ForbiddenIslandPanel.java
@@ -1,4 +1,12 @@
 
 public class ForbiddenIslandPanel {
-
+	private int seed, waterLevel, numPlayers;
+  //add other variables as needed
+  
+	public ForbiddenIslandPanel(int s, int w, int p) {
+		seed=s;
+		waterLevel=w;
+		numPlayers=p;
+		//add everything else that happens in the constructor (written by sampadaa?)
+	}
 }
