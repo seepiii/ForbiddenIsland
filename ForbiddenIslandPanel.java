3bf5de25567eb8cc5f745b342e3336d758011bcf
seepiii <93924225+seepiii@users.noreply.github.com>
2021-11-09 07:20:54 -0600

    Update ForbiddenIslandPanel.java

--- ForbiddenIslandPanel.java
+++ ForbiddenIslandPanel.java
@@ -1,5 +1,20 @@
-
-public class ForbiddenIslandPanel {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Map;
+import java.awt.Font;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+ 
+public class ForbiddenIslandPanel extends JPanel /*implements mouse*/ {
+	private BufferedImage treasureCardBack, floodCardBack;
+	private Map<String, BufferedImage> gameSquares;
 	private int seed, waterLevel, numPlayers;
   //add other variables as needed
   
@@ -8,5 +23,17 @@ public class ForbiddenIslandPanel {
 		waterLevel=w;
 		numPlayers=p;
 		//add everything else that happens in the constructor (written by sampadaa?)
+		try {
+			treasureCardBack = ImageIO.read(new File("TreasureCardBack.jpg"));
+			//floodCardBack = ImageIO.read(ForbiddenIslandPanel.class.getResource("/Image/Flood_Card_Back@2x.png"));
+		}
+		catch(Exception E) {
+			System.out.println("Exception Error");
+			return;
+		}
+	}
+public void paint(Graphics g) {
+		g.drawImage(treasureCardBack, 450, 500, 100, 120, null);
 	}
 }
+
