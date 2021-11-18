8dbb74427f5778dff9e952510c68b1a801a2ebfc
seepiii <93924225+seepiii@users.noreply.github.com>
2021-11-18 07:24:22 -0600

    Update ForbiddenIslandRunner.java

--- ForbiddenIslandRunner.java
+++ ForbiddenIslandRunner.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 
 public class ForbiddenIslandRunner {
-	private int seed, waterLevel, numPlayers;
+	private int seed;
+	private static int waterLevel;
+	private int numPlayers;
 	public static void main(String[] args) {
 		int seed = Integer.parseInt(JOptionPane.showInputDialog(null,"Please enter the seed.","Seed",JOptionPane.QUESTION_MESSAGE));
 		String waterLevel = JOptionPane.showInputDialog(null,"Please enter the water level you want to start the game at (2 - 5).","Water Level",JOptionPane.QUESTION_MESSAGE);
@@ -14,7 +16,11 @@ public class ForbiddenIslandRunner {
 		while(numPlayers < 2 || numPlayers > 4 || (numPlayers != (int)numPlayers)) {
 			JOptionPane.showMessageDialog(null, "Invalid input. Please submit a number within range.");
 			numPlayers = Integer.parseInt(JOptionPane.showInputDialog(null,"Please enter the number of players. (2 - 4)","Number of Players",JOptionPane.QUESTION_MESSAGE));
-		}
-		ForbiddenIslandFrame fif=new ForbiddenIslandFrame("Forbidden Island", seed, newWaterLevel, numPlayers);
+}
+ForbiddenIslandFrame b = new ForbiddenIslandFrame("hi", waterLevel);
+}
+	public static int getWaterLevel() {
+		return waterLevel;
 	}
+	
 }
