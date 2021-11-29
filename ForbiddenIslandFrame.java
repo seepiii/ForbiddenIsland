1df7e2bef69272353427d6ea39640c466462e968
seepiii <93924225+seepiii@users.noreply.github.com>
2021-11-28 23:00:28 -0600

    Update ForbiddenIslandFrame.java

--- ForbiddenIslandFrame.java
+++ ForbiddenIslandFrame.java
@@ -1,9 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 public class ForbiddenIslandFrame extends JFrame {
-	private static final int WIDTH = 1600;
-	private static final int HEIGHT = 960;
-	public ForbiddenIslandFrame(String title, int s, int w, int p) {
+	private static final int WIDTH = 1350;
+	private static final int HEIGHT = 740;
+	public ForbiddenIslandFrame(String title, int s, String w, int p) {
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(WIDTH, HEIGHT);
