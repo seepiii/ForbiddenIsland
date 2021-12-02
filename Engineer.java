9c6a090c910bc1534a916950f5be9b97469a934a
SamuelLi9087 <89850593+SamuelLi9087@users.noreply.github.com>
2021-12-02 14:57:26 -0600

    Update Engineer.java

--- Engineer.java
+++ Engineer.java
@@ -1,10 +1,13 @@
-
-/*public class Engineer {
+public class Engineer {
 	private int[] engCoords;
-		public Engineer() {
+		public Engineer(int[] coords) {
+			engCoords = coords;
+		}
+		
+		//shoreUp will see if the two set of coordinates are able to shore up
+		public boolean shoreUp(int[] firstTileShoreUp, int[] secondTileShoreUp) {
+			
+			//not done with shore up stuff, 
 			
 		}
-		public boolean shoreUp(int[] coords) {
-			if (Math.abs(coords[i]- )
-		}
-}*/
+}
