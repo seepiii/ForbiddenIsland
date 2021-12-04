c2d0496f7991ce98c51b77807b39206af4828cdb
nitheeshcpu <nitishkoda@outlook.com>
2021-12-03 21:52:05 -0600

    Created and Randomized Tiles objects

--- ForbiddenIslandRunner.java
+++ ForbiddenIslandRunner.java
@@ -12,13 +12,12 @@ public class ForbiddenIslandRunner {
 //			JOptionPane.showMessageDialog(null, "Invalid input. Please submit a number within range.");
 //			newWaterLevel = Integer.parseInt(JOptionPane.showInputDialog(null,"Please enter the water level you want to start the game at (2 - 5).","Water Level",JOptionPane.QUESTION_MESSAGE));
 //		}
-//		String waterLevel = JOptionPane.showInputDialog(null,"Please enter the difficulty you want to start the game at. Options: Novice, Normal, Elite, Legendary.","Difficulty",JOptionPane.QUESTION_MESSAGE);
-//		while (!waterLevel.equals("Novice")&&!waterLevel.equals("Normal")&&!waterLevel.equals("Elite")&&!waterLevel.equals("Legendary")) {
-//			JOptionPane.showMessageDialog(null, "Invalid input. Please type the option as it's given.");
-//			waterLevel = JOptionPane.showInputDialog(null,"Please enter the difficulty you want to start the game at. Options: Novice, Normal, Elite, Legendary.","Difficulty",JOptionPane.QUESTION_MESSAGE);
-//		}
-//		ForbiddenIslandFrame b = new ForbiddenIslandFrame("Forbidden Island", waterLevel);
-		MenuPanel b = new MenuPanel();
+		String waterLevel = JOptionPane.showInputDialog(null,"Please enter the difficulty you want to start the game at. Options: Novice, Normal, Elite, Legendary.","Difficulty",JOptionPane.QUESTION_MESSAGE);
+		while (!waterLevel.equals("Novice")&&!waterLevel.equals("Normal")&&!waterLevel.equals("Elite")&&!waterLevel.equals("Legendary")) {
+			JOptionPane.showMessageDialog(null, "Invalid input. Please type the option as it's given.");
+			waterLevel = JOptionPane.showInputDialog(null,"Please enter the difficulty you want to start the game at. Options: Novice, Normal, Elite, Legendary.","Difficulty",JOptionPane.QUESTION_MESSAGE);
+		}
+		ForbiddenIslandFrame b = new ForbiddenIslandFrame("Forbidden Island", waterLevel);
 
 	/*	int numPlayers = Integer.parseInt(JOptionPane.showInputDialog(null,"Please enter the number of players. (2 - 4)","Number of Players",JOptionPane.QUESTION_MESSAGE));
 		while(numPlayers < 2 || numPlayers > 4 || (numPlayers != (int)numPlayers)) {
