8cd66a96de64cad6bb4b6765680f99f6ac93fc86
SamuelLi9087 <89850593+SamuelLi9087@users.noreply.github.com>
2021-12-04 19:28:42 -0600

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -98,9 +98,7 @@ public class GameState {
 				"Watchtower", "CopperGate", "CliffsOfAbandon", "WhisperingGarden", "TempleOfTheSun",
 				"CoralPalace", "GoldGate", "FoolsLanding", "HowlingGarden", "BronzeGate"};
 
-		List<String> tileShuffle = Arrays.asList(allTiles);
-		Collections.shuffle(tileShuffle);
-		allTiles = tileShuffle.toArray(new String[tileShuffle.size()]);
+		//shuffle
 
 		int[][] pos = new int[][]{
 				{0, 2}, {0, 3}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {5, 2}, {5, 3}
