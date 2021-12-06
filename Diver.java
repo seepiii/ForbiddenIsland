4cc460b98b07707019742283284f0d2247bb7336
seepiii <93924225+seepiii@users.noreply.github.com>
2021-12-05 18:24:03 -0600

    Update Diver.java

--- Diver.java
+++ Diver.java
@@ -1,4 +1,21 @@
-
 public class Diver {
-
+	//private static int[] coordinates;
+	public Diver() {
+		//coordinates = new int[cs.length];
+		//for (int i = 0; i < cs.length; i++) {
+		//	coordinates[i] = cs[i];
+		//}
+	}
+	public boolean shoreUp() {
+		return true;
+	}
+	public static boolean move() {
+		if (ForbiddenIslandPanel.distanceX() <= 130 && ForbiddenIslandPanel.distanceY() <= 130) {
+			return true;
+		}
+		return false;
+	}
+	public String toString () {
+		return ""+ ForbiddenIslandPanel.distanceX();
+	}
 }
